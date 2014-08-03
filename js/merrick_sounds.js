d734dea668b60a09dc04832b3326a023af73d8a3
Jared Fishman <jfish@gigbear.com>
2014-08-03 00:34:12 -0700

    soundcloud plays same song no matter which song link you click

--- js/merrick_sounds.js
+++ js/merrick_sounds.js
@@ -1,35 +1,18 @@
-          function playSomeMusic(genre) {
-            console.log(genre);
-            SC.get('/tracks', {
-              genres: genre,
-              bpm: {
-                from: 120
-              }
-            }, function(tracks) {
-              var random = Math.floor(Math.random() * 49);
-              var track = tracks[random];
-              // SC.oEmbed(track.uri, { auto_play: true }, document.getElementById('target'));
+function playSong(){
+  SC.stream("/tracks/103512119", function(sound){
+    sound.play();
+  });
+}
+
+window.onload = function() {
+  SC.initialize({
+    client_id: 'cac82bb56f5d925de0499e607178dd45'
+  });
 
 
+  $('.song_panel').bind('click', function(e) {
+    e.preventDefault();
+    playSong();
+  });
+};
 
-
-            });
-          }
-
-          window.onload = function() {
-            SC.initialize({
-              client_id: 'cac82bb56f5d925de0499e607178dd45'
-            });
-
-            // SC.stream("/tracks/293", function(sound){
-            //   sound.play();
-            // });
-
-            var menuLinks = document.getElementsByClassName('genre');
-            for (var i = 0; i < menuLinks.length; i++) {
-              menuLinks[i].onclick = function(e) {
-                e.preventDefault();
-                playSomeMusic(this.innerHTML);
-              };
-            }
-          };
