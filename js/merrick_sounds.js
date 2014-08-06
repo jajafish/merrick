812ae4a8f80cf973cbf119c1bc14b3fcde8b293d
Jared Fishman <jfish@gigbear.com>
2014-08-05 20:36:26 -0700

    cool looking but largely impractical song viewer

--- js/merrick_sounds.js
+++ js/merrick_sounds.js
@@ -14,11 +14,11 @@ window.onload = function() {
     e.preventDefault();
     $('body').addClass("loading");
 
-    
-
+  
     var loadingGif = "http://38.media.tumblr.com/6a24ed04eb3abeeddec6174a84f22010/tumblr_n9tka96PXV1s2yegdo1_r1_500.gif";
     $('#merricksActualStudioImg').attr('src', loadingGif);
     $('#merrick_studio_pic, #tracks').addClass('hidden');
+    $('#currentTrackTitle').text("Around");
 
     playSong();
     window.isSoundPlaying = true;
