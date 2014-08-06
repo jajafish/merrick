64622c353900f0fc37f97db7075883002aba9fcd
Jared Fishman <jfish@gigbear.com>
2014-08-05 20:29:46 -0700

    pressing play removes track list and studio photo

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
 $(document).ready(function(){
-    var image = $("#playButton").children("img");
-    $("#playButton")
-        .mouseover(function() { 
-            var src = "/img/icons/playIconOnHover.png";
-            image.attr("src", src);
-        })
-        .mouseout(function() {
-            var src = "/img/icons/playIcon.png";
-            image.attr("src", src);
-        });
 
-    if ($('body').hasClass( "loading" ) ) {
- 
-        console.log('loading');
- 
-    }
+
 
 });
 
--- js/merrick_sounds.js
+++ js/merrick_sounds.js
@@ -14,9 +14,11 @@ window.onload = function() {
     e.preventDefault();
     $('body').addClass("loading");
 
+    
+
     var loadingGif = "http://38.media.tumblr.com/6a24ed04eb3abeeddec6174a84f22010/tumblr_n9tka96PXV1s2yegdo1_r1_500.gif";
-    var notLoadingGif = "/img/albumArt/elite8.jpg";
     $('#merricksActualStudioImg').attr('src', loadingGif);
+    $('#merrick_studio_pic, #tracks').addClass('hidden');
 
     playSong();
     window.isSoundPlaying = true;
